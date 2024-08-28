d8be091a9b00ae743a8199bd351e8de515ce1db8
Vishnu Jangid <103036831+vj2303@users.noreply.github.com>
2024-08-28 13:49:47 +0530

    Add files via upload

---
+++ src/app/all-partners/page.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import Navbar from "@/components/Navbar"
+import Salons from "./Salons"
+import Footer from "@/components/Footer"
+import DownloadPopUp from "@/components/DownloadPopUp"
+import ScrollToTop from "@/components/ScrollToTop"
+
+const AllSalons = () => {
+  return (
+    <div>
+      <Navbar />
+      <Salons />
+      <Footer />
+      <ScrollToTop />
+      <DownloadPopUp />
+    </div>
+  )
+}
+
+export default AllSalons
